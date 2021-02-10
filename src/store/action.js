d40e5e5edd71d0b1527984978d85286fcd7ae616
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2021-02-10 15:37:30 +0100

    Update action.js

--- src/store/action.js
+++ src/store/action.js
@@ -1,1 +1,7 @@
 export const CLICK = 'CLICK'
+export const FECTH_POKEMON_SUCCESS = "FECTH_POKEMON_SUCCESS"
+
+export const fetchPokemonSuccess = (pokemons) => ({
+    type: FECTH_POKEMON_SUCCESS,
+    pokemons
+})
