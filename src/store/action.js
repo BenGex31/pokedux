f062c9d3f29adb97aaab88f8fd91489822c96485
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2021-02-11 12:00:22 +0100

    Update action.js

--- src/store/action.js
+++ src/store/action.js
@@ -1,6 +1,7 @@
 export const CLICK = 'CLICK'
 export const FECTH_POKEMON_SUCCESS = "FECTH_POKEMON_SUCCESS"
 export const FECTH_POKEMON_PENDING = "FECTH_POKEMON_PENDING"
+export const SHOW_POKEMON = "SHOW_POKEMON"
 
 export const fetchPokemonSuccess = (pokemons) => ({
     type: FECTH_POKEMON_SUCCESS,
@@ -10,3 +11,12 @@ export const fetchPokemonSuccess = (pokemons) => ({
 export const fetchPokemonPending = (pokemons) => ({
     type: FECTH_POKEMON_PENDING
 })
+
+export const showPokemonAction = pokemons => {
+    const random = Math.floor(Math.random() * 10)
+    const onScreen = pokemons[random]
+    
+    return dispatch => {
+        dispatch({ type: SHOW_POKEMON, onScreen })
+    }
+}
