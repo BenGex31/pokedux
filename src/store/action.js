083441ce6fe3d72feb9961fab062c79ab25aaeb4
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2021-02-11 15:07:39 +0100

    Update action.js

--- src/store/action.js
+++ src/store/action.js
@@ -1,22 +1,36 @@
-export const CLICK = 'CLICK'
-export const FECTH_POKEMON_SUCCESS = "FECTH_POKEMON_SUCCESS"
-export const FECTH_POKEMON_PENDING = "FECTH_POKEMON_PENDING"
-export const SHOW_POKEMON = "SHOW_POKEMON"
+export const CLICK = 'CLICK';
+export const FECTH_POKEMON_SUCCESS = "FECTH_POKEMON_SUCCESS";
+export const FECTH_POKEMON_PENDING = "FECTH_POKEMON_PENDING";
+export const SHOW_POKEMON = "SHOW_POKEMON";
+export const CATCH_POKEMON = "CATCH_POKEMON";
+
 
 export const fetchPokemonSuccess = (pokemons) => ({
     type: FECTH_POKEMON_SUCCESS,
     pokemons
-})
+});
 
 export const fetchPokemonPending = (pokemons) => ({
     type: FECTH_POKEMON_PENDING
-})
+});
 
 export const showPokemonAction = pokemons => {
-    const random = Math.floor(Math.random() * 10)
-    const onScreen = pokemons[random]
+    const filteredPokemons = pokemons.filter(pokemon => !pokemon.isCatch);
+    const max = filteredPokemons.length;
+    const random = Math.floor(Math.random() * max);
+
+    const onScreen = filteredPokemons[random];
     
     return dispatch => {
-        dispatch({ type: SHOW_POKEMON, onScreen })
-    }
-}
+        dispatch({ type: SHOW_POKEMON, onScreen });
+    };
+};
+
+export const catchPokemonAction = () => {
+    console.log('catch');
+    const random = Math.floor(Math.random() * 255);
+    
+    return dispatch => {
+        dispatch({ type: CATCH_POKEMON, random });
+    };
+};
