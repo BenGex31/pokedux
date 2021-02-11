22f9bfdd59d31d54cb12bd5b754555eae7557000
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2021-02-11 15:07:27 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { useEffect } from "react";
 import "./styles.css";
 import { connect } from "react-redux";
 
-import { CLICK, showPokemonAction, SHOW_POKEMON } from "./store/action";
+import { CLICK, showPokemonAction, catchPokemonAction } from "./store/action";
 import fetchPokemons from './store/fetchPokemon'
 
 import GameBoy from "./components/GameBoy";
 import PokeList from "./components/PokeList";
 import Loader from "./components/Loader";
 
-const App = ({ handleClick, fetchPokemons, pending, showPokemon, pokemons }) => {
+const App = ({ handleClick, fetchPokemons, pending, showPokemon, pokemons, catchPokemon }) => {
   useEffect(() => {
     fetchPokemons()
   }, [fetchPokemons])
+
+  useEffect(() => {
+    console.log(pokemons);
+  }, [pokemons])
 
   if (pending) {
     return <Loader/>
@@ -21,7 +25,9 @@ const App = ({ handleClick, fetchPokemons, pending, showPokemon, pokemons }) => 
   return (
     <div className="App">
       <button onClick={() => handleClick()}>click</button>
-      <GameBoy showPokemon={() => showPokemon(pokemons)} />
+      <GameBoy 
+        showPokemon={() => showPokemon(pokemons)} 
+        catchPokemon={catchPokemon} />
       <PokeList />
     </div>
   );
@@ -38,8 +44,13 @@ const mapDispatchToProps = dispatch => {
   return {
     fetchPokemons: () => dispatch(fetchPokemons()),
     handleClick: () => dispatch({ type: CLICK }),
-    showPokemon: pokemons => dispatch(showPokemonAction(pokemons))
+    showPokemon: pokemons => dispatch(showPokemonAction(pokemons)),
+    catchPokemon: pokemons => dispatch(catchPokemonAction())
+
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+  )(App);
